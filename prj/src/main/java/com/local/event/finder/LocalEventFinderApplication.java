5b0f267f3df8c0fd97cfcaed8bd1e230ac6c6809
Uladzislau Mikhayevich <uladzislau.mikhayevich@stud.esdc.lt>
2026-03-25 15:47:53 +0200

    feat: main
    setup idea project

---
+++ prj/src/main/java/com/local/event/finder/LocalEventFinderApplication.java
@@ -0,0 +1,11 @@
+package com.local.event.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalEventFinderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LocalEventFinderApplication.class, args);
+    }
+}
