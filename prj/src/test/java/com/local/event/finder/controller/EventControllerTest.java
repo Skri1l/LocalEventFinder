af7f4ed8a88ecd46c6ce8947101d8f728f2c2b7e
Skrill <ivan.samal@student.ehu.lt>
2026-04-08 19:27:27 +0300

    tests was added

--- prj/src/test/java/com/local/event/finder/controller/EventControllerTest.java
+++ prj/src/test/java/com/local/event/finder/controller/EventControllerTest.java
@@ -15,12 +15,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -352,6 +359,7 @@ public class EventControllerTest {
         event.setMaxParticipants(15);
         event.setAgeRestriction(19);
         event.setImageUrl("https://test.com/image.png");
+        event.setCreatedBy(user);
 
         Event savedEvent = eventRepository.save(event);
 
@@ -361,5 +369,167 @@ public class EventControllerTest {
                 .andExpect(jsonPath("$.title").value("newTest"))
                 .andExpect(jsonPath("$.city").value("newCity"))
                 .andExpect(jsonPath("$.country").value("newCountry"));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void getEventByIdTestShouldReturnNotFoundWhenEventDoesNotExist() throws Exception {
+
+        mockMvc.perform(get(EVENT_URL + "/{id}", 999L))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void getEventsByTitleShouldReturnAllEventsWhenSeveralEventsHaveSameTitle() throws Exception {
+        User user = userRepository.findByUsername("testuser").orElseThrow();
+        Event firstEvent = new Event();
+        firstEvent.setTitle("newTest");
+        firstEvent.setDescription("first description");
+        firstEvent.setLatitude(60.0);
+        firstEvent.setLongitude(60.0);
+        firstEvent.setCountry("newCountry");
+        firstEvent.setCity("newCity");
+        firstEvent.setStartTime(LocalDateTime.of(2026, 4, 1, 17, 0));
+        firstEvent.setEndTime(LocalDateTime.of(2026, 4, 1, 19, 30));
+        firstEvent.setMaxParticipants(15);
+        firstEvent.setAgeRestriction(19);
+        firstEvent.setImageUrl("https://test.com/image1.png");
+        firstEvent.setCreatedBy(user);
+
+        Event secondEvent = new Event();
+        secondEvent.setTitle("newTest");
+        secondEvent.setDescription("second description");
+        secondEvent.setLatitude(61.0);
+        secondEvent.setLongitude(61.0);
+        secondEvent.setCountry("newCountry2");
+        secondEvent.setCity("newCity2");
+        secondEvent.setStartTime(LocalDateTime.of(2026, 4, 2, 17, 0));
+        secondEvent.setEndTime(LocalDateTime.of(2026, 4, 2, 19, 30));
+        secondEvent.setMaxParticipants(20);
+        secondEvent.setAgeRestriction(18);
+        secondEvent.setImageUrl("https://test.com/image2.png");
+        secondEvent.setCreatedBy(user);
+
+        eventRepository.save(firstEvent);
+        eventRepository.save(secondEvent);
+
+        mockMvc.perform(get(EVENT_URL + "/title/{title}", "newTest"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[*].title", everyItem(is("newTest"))))
+                .andExpect(jsonPath("$[*].city", containsInAnyOrder("newCity", "newCity2")));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void getEventsByTitleShouldReturnEmptyListWhenEventDoesNotExist() throws Exception {
+        mockMvc.perform(get(EVENT_URL + "/title/{title}", "notFoundTet"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void getAllEventsShouldReturnAllEvents()  throws Exception {
+        User user = userRepository.findByUsername("testuser").orElseThrow();
+        Event firstEvent = new Event();
+        firstEvent.setTitle("Event1");
+        firstEvent.setDescription("first description");
+        firstEvent.setLatitude(60.0);
+        firstEvent.setLongitude(60.0);
+        firstEvent.setCountry("newCountry");
+        firstEvent.setCity("newCity");
+        firstEvent.setStartTime(LocalDateTime.of(2026, 4, 1, 17, 0));
+        firstEvent.setEndTime(LocalDateTime.of(2026, 4, 1, 19, 30));
+        firstEvent.setMaxParticipants(15);
+        firstEvent.setAgeRestriction(19);
+        firstEvent.setImageUrl("https://test.com/image1.png");
+        firstEvent.setCreatedBy(user);
+
+        Event secondEvent = new Event();
+        secondEvent.setTitle("Event2");
+        secondEvent.setDescription("second description");
+        secondEvent.setLatitude(61.0);
+        secondEvent.setLongitude(61.0);
+        secondEvent.setCountry("newCountry2");
+        secondEvent.setCity("newCity2");
+        secondEvent.setStartTime(LocalDateTime.of(2026, 4, 2, 17, 0));
+        secondEvent.setEndTime(LocalDateTime.of(2026, 4, 2, 19, 30));
+        secondEvent.setMaxParticipants(20);
+        secondEvent.setAgeRestriction(18);
+        secondEvent.setImageUrl("https://test.com/image2.png");
+        secondEvent.setCreatedBy(user);
+
+        eventRepository.save(firstEvent);
+        eventRepository.save(secondEvent);
+
+        mockMvc.perform(get(EVENT_URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[*].title", containsInAnyOrder("Event1", "Event2")));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void getAllEventsShouldReturnEmptyListWhenEventDoesNotExist() throws Exception {
+        mockMvc.perform(get(EVENT_URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void updateEventByIdTestShouldReturnUpdatedEvent() throws Exception {
+        User user = userRepository.findByUsername("testuser").orElseThrow();
+        Event oldEvent = new Event();
+        oldEvent.setTitle("oldEvent");
+        oldEvent.setDescription("old description");
+        oldEvent.setLatitude(60.0);
+        oldEvent.setLongitude(60.0);
+        oldEvent.setCountry("oldCountry");
+        oldEvent.setCity("oldCity");
+        oldEvent.setStartTime(LocalDateTime.of(2026, 4, 1, 17, 0));
+        oldEvent.setEndTime(LocalDateTime.of(2026, 4, 1, 19, 30));
+        oldEvent.setMaxParticipants(15);
+        oldEvent.setAgeRestriction(19);
+        oldEvent.setImageUrl("https://oldtest.com/image1.png");
+        oldEvent.setCreatedBy(user);
+
+        Event savedEvent = eventRepository.save(oldEvent);
+
+        Long id = savedEvent.getId();
+
+        Event newEvent = new Event();
+        newEvent.setTitle("newEvent");
+        newEvent.setDescription("new description");
+        newEvent.setLatitude(61.0);
+        newEvent.setLongitude(61.0);
+        newEvent.setCountry("newCountry");
+        newEvent.setCity("newCity");
+        newEvent.setStartTime(LocalDateTime.of(2026, 4, 2, 17, 0));
+        newEvent.setEndTime(LocalDateTime.of(2026, 4, 2, 19, 30));
+        newEvent.setMaxParticipants(20);
+        newEvent.setAgeRestriction(18);
+        newEvent.setImageUrl("https://newtest.com/image.png");
+        newEvent.setCreatedBy(user);
+
+        mockMvc.perform(put(EVENT_URL + "/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newEvent)))
+                .andExpect(status().isNoContent());
+
+        Event updatedEvent = eventRepository.findById(id).orElseThrow();
+
+        assertEquals("newEvent", updatedEvent.getTitle());
+        assertEquals("new description", updatedEvent.getDescription());
+        assertEquals("newCountry", updatedEvent.getCountry());
+        assertEquals("newCity", updatedEvent.getCity());
+        assertEquals(61.0, updatedEvent.getLatitude());
+        assertEquals(61.0, updatedEvent.getLongitude());
     }
 }
