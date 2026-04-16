66cbaa83e711611bf1e258906ccdfdafbffab50d
Uladzislau Mikhayevich <uladzislau.mikhayevich@stud.esdc.lt>
2026-04-16 09:09:34 +0300

    chore: #72 (user registration tests)
    running tests
    different database for tests

--- prj/src/test/java/com/local/event/finder/controller/AuthControllerTest.java
+++ prj/src/test/java/com/local/event/finder/controller/AuthControllerTest.java
@@ -2,6 +2,8 @@ package com.local.event.finder.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.local.event.finder.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,11 +14,9 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Map;
 
-import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -28,12 +28,23 @@ public class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    /**
+     * In test uses only for cleaning all users in test database.
+     */
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void clean() {
+        userRepository.deleteAll();
+    }
+
     @Test
     void shouldRegisterUserSuccessfully() throws Exception {
         Map<String, Object> request = Map.of(
-                "username", "john_doe",
-                "email", "john@example.com",
-                "password", "StrongPassword123!",
+                "username", "testUsername",
+                "email", "john3@example.com",
+                "password", "av2bB123?0",
                 "avatar_url", "https://example.com/avatar.png",
                 "age", 25
         );
@@ -41,23 +52,23 @@ public class AuthControllerTest {
         MvcResult result = mockMvc.perform(post("/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isOk())
+                .andExpect(status().isCreated())
                 .andReturn();
 
         String json = result.getResponse().getContentAsString();
         JsonNode root = objectMapper.readTree(json);
 
         JsonNode data = root.get("data");
-        assertNotNull(data, "Response должен содержать поле 'data'");
+        assertNotNull(data, "Response should have field 'data'");
 
         assertTrue(data.has("access_token"),
-                "В ответе отсутствует access_token");
+                "Answer doesn't have access_token");
 
         assertTrue(data.has("refresh_token"),
-                "В ответе отсутствует refresh_token");
+                "В doesn't have refresh_token");
 
         int expiresIn = data.get("expires_in").asInt();
         assertTrue(expiresIn > 0,
-                "expires_in должен быть > 0, но был: " + expiresIn);
+                "expires_in should be > 0, but was: " + expiresIn);
     }
 }
