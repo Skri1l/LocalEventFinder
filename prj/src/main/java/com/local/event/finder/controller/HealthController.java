2066b1eacd6cb851d33c1fd9874f37c2a71e20f7
Uladzislau Mikhayevich <uladzislau.mikhayevich@stud.esdc.lt>
2026-04-27 11:13:26 +0300

    feat: #72 (user registration tests)
    add comment

--- prj/src/main/java/com/local/event/finder/controller/HealthController.java
+++ prj/src/main/java/com/local/event/finder/controller/HealthController.java
@@ -7,7 +7,9 @@ import java.util.logging.Logger;
 
 @RestController
 public class HealthController {
-
+    /* COMMENT: I recommend to not using statics fields when use java with spring boot.
+    * Spring Boot gives u bean system that works automatically and it controls them lifecycle,
+    * and when u use static fields in classes they saves however object deletes. */
     private final static Logger logger = Logger.getLogger(HealthController.class.getName());
 
     @GetMapping("/health")
