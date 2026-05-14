cefb51d0f0a6d35ae178a3a953a8895ba548e309
Uladzislau Mikhayevich <uladzislau.mikhayevich@stud.esdc.lt>
2026-05-14 08:22:31 +0300

    feat: cors config
    add port 5174

--- prj/src/main/java/com/local/event/finder/security/CorsConfiguration.java
+++ prj/src/main/java/com/local/event/finder/security/CorsConfiguration.java
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    private static final String FRONTEND_PORT = "5173";
+    private static final String FRONTEND_PORT1 = "5173";
+    private static final String FRONTEND_PORT2 = "5174";
     private static final String FRONTEND_DOMAIN = "ec2-51-20-182-189.eu-north-1.compute.amazonaws.com";
     private static final String FRONTEND_IP = "51.20.182.189";
 
@@ -15,9 +16,12 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:" + FRONTEND_PORT,
-                        FRONTEND_DOMAIN + ":" + FRONTEND_PORT,
-                        FRONTEND_IP + ":" + FRONTEND_PORT
+                        "http://localhost:" + FRONTEND_PORT1,
+                        FRONTEND_DOMAIN + ":" + FRONTEND_PORT1,
+                        FRONTEND_IP + ":" + FRONTEND_PORT1,
+                        "http://localhost:" + FRONTEND_PORT2,
+                        FRONTEND_DOMAIN + ":" + FRONTEND_PORT2,
+                        FRONTEND_IP + ":" + FRONTEND_PORT2
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
